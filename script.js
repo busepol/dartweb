729162a37678d81e259f7bd68ba34115bd177e86
busepol <busetasdemir174@gmail.com>
2024-12-10 15:08:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 // Example: Log a message when the page loads
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('D.A.R.T Website loaded!');
+const menuButton = document.getElementById('menu-button');
+const sidebar = document.getElementById('sidebar');
+const body = document.body;
+const overlay = document.getElementById("overlay");
+
+menuButton.addEventListener('click', function() {
+    overlay.classList.toggle("visible");
+    sidebar.classList.toggle('open'); 
+});
+
+body.addEventListener('click', function(event) {
+    if (sidebar.classList.contains('open') && !sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+      overlay.classList.remove("visible");
+      sidebar.classList.remove('open'); 
+    }
+    
   });
+
+ 
+  sidebar.addEventListener('click', function(event) {
+    event.stopPropagation(); 
+
+});
+  
   
